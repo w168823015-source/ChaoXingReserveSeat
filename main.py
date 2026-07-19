39bb93d21dae60e565cfd8e0eff59ff246019a5e
w168823015-source <w168823015@gmail.com>
2026-07-20 01:17:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ ENDTIME = "12:01:00"  # 根据学校的预约座位时间+1min即可
 
 ENABLE_SLIDER = True  # 是否有滑块验证
 MAX_ATTEMPT = 5  # 最大尝试次数
-RESERVE_NEXT_DAY = False  # 预约明天而不是今天的
+RESERVE_NEXT_DAY = True  # 预约明天而不是今天的
 
 
 def login_and_reserve(users, usernames, passwords, action, success_list=None):
